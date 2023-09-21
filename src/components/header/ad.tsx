a4fc5d31ca8db0fafcca82df51f358e96d633674
DiNeez <dneez86@gmail.com>
2023-09-21 15:26:06 +0300

    add banner to header

--- src/components/header/ad.tsx
+++ src/components/header/ad.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import { BannerButton } from "../ui";
-import Image from "next/image";
 
 export default function Ad() {
   return (
     <div className={styles.add}>
-      <Image
-        src="/images/main-banner-bg.jpg"
-        alt="banner"
-        width={970}
-        height={90}
-        className={styles.addBackground}
-      />
       <div className={styles.container}>
         <div className={styles.yellow}>Ready-to-use Cupons</div>
         <div className={styles.white}>Save 200$ bonus cupon</div>
